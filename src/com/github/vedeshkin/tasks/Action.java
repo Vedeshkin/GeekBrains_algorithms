f30fb7a0f6fa110bcb4df82f0a7cf2fe45d0f495
vvedeshkin <evilfunt@gmail.com>
2018-11-29 23:42:46 +0300

    Done the homework

---
+++ src/com/github/vedeshkin/tasks/Action.java
@@ -0,0 +1,9 @@
+package com.github.vedeshkin.tasks;
+
+/**
+ * Created by Vedeshkin on 11/29/2018.
+ * All right reserved.
+ */
+public interface Action {
+     void performAction(int [] arr);
+    }
